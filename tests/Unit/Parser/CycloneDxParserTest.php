47996c0dbadcf86f1bac1cf773ec883725392b36
Martin Adler <mteu@mailbox.org>
2026-09-25 22:09:39 +0200

    [BUGFIX] Model license acknowledgement

--- tests/Unit/Parser/CycloneDxParserTest.php
+++ tests/Unit/Parser/CycloneDxParserTest.php
@@ -28,6 +28,7 @@ use mteu\SbomParser\Entity\Bom;
 use mteu\SbomParser\Entity\Component;
 use mteu\SbomParser\Entity\ComponentType;
 use mteu\SbomParser\Entity\Dependency;
+use mteu\SbomParser\Entity\LicenseAcknowledgement;
 use mteu\SbomParser\Entity\OrganizationalContact;
 use mteu\SbomParser\Exception\SbomParseException;
 use mteu\SbomParser\Parser\Configuration\CycloneDxParserOptions;
@@ -708,6 +709,86 @@ final class CycloneDxParserTest extends TestCase
         self::assertSame('MIT', $license->text->content);
         self::assertNull($license->text->contentType);
         self::assertNull($license->text->encoding);
+    }
+
+    #[Test]
+    public function parseFromArrayHydratesLicenseAcknowledgement(): void
+    {
+        $bom = $this->subject->parseFromArray([
+            'bomFormat' => 'CycloneDX',
+            'specVersion' => '1.6',
+            'components' => [
+                [
+                    'type' => 'library',
+                    'name' => 'licensed-component',
+                    'licenses' => [
+                        [
+                            'license' => ['id' => 'MIT', 'acknowledgement' => 'declared'],
+                        ],
+                        [
+                            'expression' => 'Apache-2.0 OR MIT',
+                            'acknowledgement' => 'concluded',
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+
+        $licenses = $bom->components[0]->licenses ?? [];
+
+        self::assertCount(2, $licenses);
+        self::assertSame(LicenseAcknowledgement::DECLARED, $licenses[0]->license?->acknowledgement);
+        self::assertSame(LicenseAcknowledgement::CONCLUDED, $licenses[1]->acknowledgement);
+    }
+
+    #[Test]
+    public function parseFromArrayLeavesLicenseAcknowledgementNullWhenAbsent(): void
+    {
+        $bom = $this->subject->parseFromArray([
+            'bomFormat' => 'CycloneDX',
+            'specVersion' => '1.6',
+            'components' => [
+                [
+                    'type' => 'library',
+                    'name' => 'licensed-component',
+                    'licenses' => [['license' => ['id' => 'MIT']]],
+                ],
+            ],
+        ]);
+
+        $licenses = $bom->components[0]->licenses ?? [];
+
+        self::assertCount(1, $licenses);
+        self::assertNull($licenses[0]->license?->acknowledgement);
+        self::assertNull($licenses[0]->acknowledgement);
+    }
+
+    /**
+     * Acknowledgement was introduced in CycloneDX 1.6, so the 1.4 and 1.5
+     * fixtures legitimately carry none.
+     */
+    #[Test]
+    public function parseFromFileHydratesLicenseAcknowledgementFromFixtures(): void
+    {
+        foreach (['1.6', '1.7'] as $version) {
+            $bom = $this->subject->parseFromFile(self::fixtureDir() . "/bom-{$version}.json");
+
+            $acknowledgements = [];
+            foreach ($bom->components ?? [] as $component) {
+                foreach ($component->licenses ?? [] as $licenseChoice) {
+                    $acknowledgement = $licenseChoice->license?->acknowledgement;
+                    if ($acknowledgement !== null) {
+                        $acknowledgements[] = $acknowledgement;
+                    }
+                }
+            }
+
+            self::assertContains(
+                LicenseAcknowledgement::DECLARED,
+                $acknowledgements,
+                sprintf('bom-%s.json declares license acknowledgements that were not hydrated.', $version),
+            );
+        }
     }
 
     /** @return \Generator<string, array{string, string}> */
